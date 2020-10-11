67cb919e6b30962126a68466457fc53ed627d4f6
KorValery <71253114+KorValery@users.noreply.github.com>
2020-10-11 20:37:43 +0300

    Update JavaApplication1.java

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -18,9 +18,9 @@ public class JavaApplication1{
                 if(d[i]==17){
                     d1[i][j]=0.5*Math.exp(Math.exp(x[j]));
                 }else if(d[i]==5 || d[i]==6 || d[i]==9 || d[i]==10 || d[i]==11 || d[i]==13 || d[i]==15){
-                    d1[i][j]=Math.asin(1/(Math.exp(Math.pow((Math.acos((x[j]+4.5)/17)/4), Math.tan(Math.pow(x[j]/2, 3))))));
+                    d1[i][j]=Math.asin(1.0/(Math.exp(Math.pow((Math.acos((x[j]+4.5)/17.0)/4.0), Math.tan(Math.pow(x[j]/2.0, 3.0))))));
                 }else{
-                    d1[i][j]=Math.sin(Math.pow(2*Math.sin(Math.pow(Math.PI/x[j], 3)), 2));
+                    d1[i][j]=Math.sin(Math.pow(2.0*Math.sin(Math.pow(Math.PI/x[j], 3.0)), 2.0));
                 }
                 System.out.printf("%,5f %n", d1[i][j]);
             }
